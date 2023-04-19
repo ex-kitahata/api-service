9af865f3496b51213a0a3a16040b0390403431d9
北畑 <kitahata@exdesign.co.jp>
2023-04-19 14:46:40 +0900

    json出力を修正

--- dist/routes/jsonrouter.js
+++ dist/routes/jsonrouter.js
@@ -4,10 +4,9 @@ exports.jsonRouter = void 0;
 var jsonRouter = function (req, res) {
     var params = req.params;
     var _a = params.name, name = _a === void 0 ? "名前" : _a, _b = params.id, id = _b === void 0 ? 0 : _b;
-    return res.json({
-        name: name,
-        id: id
-    });
+    var jsonText = "{\"name\": ".concat(name, ",\"id\": ").concat(id, "}");
+    var obj = JSON.parse(JSON.stringify(jsonText));
+    return res.render('json', { title: 'json Api', text: obj });
 };
 exports.jsonRouter = jsonRouter;
 //# sourceMappingURL=jsonRouter.js.map
--- src/routes/jsonRouter.ts
+++ src/routes/jsonRouter.ts
@@ -3,10 +3,8 @@ import { Request, Response } from 'express';
 export const jsonRouter = (req: Request, res: Response) => {
     const { params } = req;
     const { name = "名前", id = 0 } = params;
-    return res.json(
-        {
-            name: name,
-            id: id
-        }
-    );
+    var jsonText: string = `{"name": ${name},"id": ${id}}`;
+    const obj = JSON.parse(JSON.stringify(jsonText));
+   
+    return res.render('json',{ title: 'json Api', text: obj});
 };
