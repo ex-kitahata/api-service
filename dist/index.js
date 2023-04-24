4758edcfda9a3ea3fa39aa2db3fab0009eebebe2
北畑 <kitahata@exdesign.co.jp>
2023-04-24 16:10:10 +0900

    テスト

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var handlers_js_1 = require("./routes/handlers.js");
 var index_js_1 = require("./routes/index.js");
+var body_parser_1 = __importDefault(require("body-parser"));
 var path = require('path');
 var app = (0, express_1.default)();
 var port = process.env.PORT || '8000';
@@ -18,6 +19,7 @@ app.use('/hello/:name', handlers_js_1.helloHandler);
 app.use('/movies/:id', index_js_1.movieRouter);
 app.use('/movies', index_js_1.moviesRouter);
 app.use('/', handlers_js_1.rootHandler);
+app.use(body_parser_1.default.json());
 app.post('/test', function (req, res) {
     console.log(req.body);
     res.send("Received POST Data!");
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { rootHandler, helloHandler } from './routes/handlers.js';
 import { addData, movieRouter, moviesRouter } from './routes/index.js';
+import bodyParser, { BodyParser } from 'body-parser';
 const path = require('path');
 
 const app = express();
@@ -17,6 +18,7 @@ app.use('/movies/:id', movieRouter);
 app.use('/movies', moviesRouter);
 app.use('/', rootHandler);
 
+app.use(bodyParser.json());
 app.post('/test', (req, res) => {
   console.log(req.body);
   res.send("Received POST Data!");
